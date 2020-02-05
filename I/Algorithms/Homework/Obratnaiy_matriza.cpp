bcd2bfd16c7f4e193160f6e7e62dd6b2979542bd
BunnyBoss75 <BunnyBoss75@users.noreply.github.com>
2020-02-05 21:04:43 +0200

    first commit

---
+++ I/Algorithms/Homework/Obratnaiy_matriza.cpp
@@ -0,0 +1,111 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <time.h>
+using namespace std;
+double def(double** A, int count) {  
+	int i, j, k, z;
+	double sum = 0;
+	if (count == 1)
+		return A[0][0];
+	else {
+		double** A1 = new double*[count - 1];
+		for (i = 0; i < count - 1; i++)
+			A1[i] = new double[count - 1];
+		for (z = 0; z < count; z++) {
+			for (i = 0, k = 0; i < count; k++, i++) {
+				if (i == z)
+					i++;
+				if (i == count)
+					break;
+				for (j = 1; j < count; j++)
+					A1[k][j - 1] = A[i][j];
+			}
+			sum += pow(-1, z) * def(A1, count - 1) * A[z][0];
+		}
+		for (i = 0; i < count - 1; i++)
+			delete[] A1[i];
+		delete[] A1;
+		return sum;
+	}
+}
+int main() {
+	int n, i, j, k, z, y, x;
+	cin >> n;
+    double** A = new double* [n];
+	for (i = 0; i < n; i++)
+		A[i] = new double[n];
+	double** At = new double*[n];
+	for (i = 0; i < n; i++)
+		At[i] = new double[n];
+	double** Ad = new double*[n - 1];
+	for (i = 0; i < n - 1; i++)
+		Ad[i] = new double[n - 1];
+	double** Ao = new double*[n];
+	for (i = 0; i < n; i++)
+		Ao[i] = new double[n];
+	double** Ae = new double*[n];
+	for (i = 0; i < n; i++)
+		Ae[i] = new double[n];
+	srand(time(NULL));
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
+			//cin >> A[i][j];
+			A[i][j] = double (rand() % 100) / 10;
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++)
+			printf("%6.1f", A[i][j]);
+		cout << endl;
+	}
+	double det = def(A, n);
+	printf("determinant: %f\n", det);
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			At[j][i] = A[i][j];
+		}
+	}
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++) {
+			for (k = 0, y = 0; k < n; k++, y++) {
+				if (k == i)
+					k++;
+				if (k == n)
+					break;
+				for (z = 0, x = 0; z < n; z++, x++) {
+					if (z == j)
+						z++;
+					if (z == n)
+						break;
+					Ad[y][x] = At[k][z];
+				}
+			}
+			Ao[i][j] = (pow(-1, i + j) * def(Ad, n - 1)) / det;
+		}
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++)
+			printf("%7.2f", Ao[i][j]);
+		cout << endl;
+	}
+	cout << endl;
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			Ae[i][j] = 0;
+			for (x = 0; x < n; x++)
+				Ae[i][j] += A[i][x] * Ao[x][j];
+		}
+	}
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			printf("%10.5f", Ae[i][j]);
+		}
+		cout << endl;
+	}
+	double eps = 10e-10;
+	bool program_good_work = true;
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			if ((i == j && abs(Ae[i][j] - 1) > eps) || (i != j && abs(Ae[i][j]) > eps))
+				program_good_work = false;
+		}
+	}
+	cout << program_good_work;
+}
---
+++ I/Prog/Sem2_lab3.cpp
@@ -0,0 +1,44 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main() {
+	system("chcp 1251");
+	setlocale(LC_ALL, "Russian");
+	int i, n, j, flag = 0;
+	char s[1000], send[1000];
+	send[999] = '\0';
+	s[999] = '\0';
+	char month[12][15] = { " января ", " февраля ", " марта ", " апреля ", " мая ", " июня ", " июля ", " августа ", " сентября ", " октября ", " ноября ", " декабря " };
+	char* s1;
+	char d[11];
+	_strset_s(send, '\0');
+	_strset_s(s, '\0');
+	cin.getline(s, 1000);
+	for (i = 0; i < 1000; i++) {
+		flag = 0;
+		s1 = &s[i];
+		if (strlen(s1) < 10)
+			break;
+		for (j = 0; j < 10; j++)
+			if (!((s1[j] - '0' < 10 && s1[j] - '0' > -1 && j != 2 && j != 5) || ((j == 2 || j == 5) && s1[j] == '.'))) {
+				flag = 1;
+				strncat_s(send, sizeof(send), s1, 1);
+				break;
+			}
+		if (flag == 1)
+			continue;
+		strncpy_s(d, sizeof(d), s1, 10);
+		strncat_s(send, d, 2);
+		n = (s1[3] - '0') * 10 + s1[4] - '0';
+		if (n == 0 || n > 12)
+			strcat_s(send, " < неверный номер месяца > ");
+		else
+			strcat_s(send, month[n - 1]);
+		strncat_s(send, &d[6], 4);
+		i += 9;
+	}
+
+	strncat_s(send, sizeof(send), s1, 9);
+	printf("%s\n", send);
+}
